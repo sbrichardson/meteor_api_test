ce239b583efe56c7ebfd362c5ba6e402ac4db09c
sbrichardson <srichardson@reactual.io>
2018-06-09 16:16:38 -0600

    add license for zeit/micro code used

--- server/api/index.js
+++ server/api/index.js
@@ -3,13 +3,14 @@ import { WebApp } from 'meteor/webapp'
 import { getJson } from './parser'
 
 WebApp.connectHandlers.use('/hello', async (req, res, next) => {
-  const json = await getJson(req).catch(e =>
+  console.info('/hello route hit, req\n', req)
+
+  const json = await getJson(req).catch(e => {
     console.error('/hello - err catch parsing json', e)
-  )
+  })
 
   json && console.info('getJson parse result:\n', json)
 
   res.writeHead(200)
-
   res.end(`Hello world from: ${Meteor.release}`)
 })
--- server/api/parser.js
+++ server/api/parser.js
@@ -1,9 +1,40 @@
-// Packages
 import contentType from 'content-type'
 import getRawBody from 'raw-body'
 
-// Maps requests to buffered raw bodies so that
-// multiple calls to `json` work as expected
+/**
+ * @file Portions of code re-used from the zeit/micro repository
+ * @see https://github.com/zeit/micro/blob/master/lib/index.js
+ * @license
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 ZEIT, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+
+/**
+ * Maps requests to buffered raw bodies so that
+ * multiple calls to `json` work as expected
+ * @type {WeakMap}
+ */
 const rawBodyMap = new WeakMap()
 
 const parseJSON = str => {
